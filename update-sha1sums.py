31a4f2b0b624220f0be340d46bc4878e854cfa26
dazza5000 <darran.kelinske@gmail.com>
2019-12-27 10:01:07 -0600

    Update vendor device to lenovo tb8504

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -19,8 +19,8 @@
 from hashlib import sha1
 import sys
 
-device='rolex'
-vendor='xiaomi'
+device='tb8504'
+vendor='lenovo'
 
 lines = [ line for line in open('proprietary-files.txt', 'r') ]
 vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
